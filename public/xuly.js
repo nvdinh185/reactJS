45c82c17b803370e5962262764e77704d8ee87e3
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-06 15:32:53 +0700

    Adding Local State to a Class

--- public/xuly.js
+++ public/xuly.js
@@ -1,28 +1,20 @@
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    );
-};
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
 
-class Clock2 extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
 };
 
-function tick() {
-    ReactDOM.render(
-        <Clock2 date={new Date()} />,
-        document.getElementById('root')
-    );
-};
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
