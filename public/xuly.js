af2cda18c793ea9251bc662a7c05e94dcb706b4f
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-06 11:06:00 +0700

    Embedding Expressions in JSX - JSX is an Expression Too

--- public/xuly.js
+++ public/xuly.js
@@ -1,7 +1,32 @@
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+}
+
+const element2 = (
+    <div>
+        {getGreeting(user)}
+    </div>
+);
 
 ReactDOM.render(
-    element,
+    element2,
     document.getElementById("root")
 );
