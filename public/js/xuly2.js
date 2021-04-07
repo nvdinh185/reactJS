614e92d5edba49d3f3af9664fe23528d93d866d0
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-07 15:49:34 +0700

    game full

---
+++ public/js/xuly2.js
@@ -0,0 +1,67 @@
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare() {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare()}{this.renderSquare()}{this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}{this.renderSquare()}{this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}{this.renderSquare()}{this.renderSquare()}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
