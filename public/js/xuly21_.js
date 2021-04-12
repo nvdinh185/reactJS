9d4c4920d24f3a8a0838e105548f7120a36ac6ae
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-12 14:15:54 +0700

    Conditional Rendering

---
+++ public/js/xuly21_.js
@@ -0,0 +1,67 @@
+function UserGreeting() {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting() {
+    return <h1>Please sign up!</h1>;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: false };
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {
+                    isLoggedIn
+                        ? <LogoutButton onClick={this.handleLogoutClick} />
+                        : <LoginButton onClick={this.handleLoginClick} />
+                }
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('root')
+);
---
+++ public/js/xuly21_2.js
@@ -0,0 +1,76 @@
+function UserGreeting() {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting() {
+    return <h1>Please sign up.</h1>;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: true };
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {
+                    "123" &&
+                    <div>
+                        <LogoutButton onClick={this.handleLogoutClick} />
+                        <LoginButton onClick={this.handleLoginClick} />
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('root')
+);
