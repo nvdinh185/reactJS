0289d346903f57b16f66c93aa08467f12de6b98f
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-14 16:09:46 +0700

    Composition and Thinking

---
+++ public/LiftingStateUp/xuly3.js
@@ -0,0 +1,53 @@
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { temperature: '' };
+    }
+
+    handleChange(e) {
+        this.setState({ temperature: e.target.value });
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                    onChange={this.handleChange} />
+                <BoilingVerdict
+                    celsius={parseFloat(this.state.temperature)} />
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    render() {
+        return (
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root')
+);
