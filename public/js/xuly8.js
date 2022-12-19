387b0648f8cf0768bdfe3d52ef05c3227f476cb0
nvdinh185 <nvdinh0766777123@gmail.com>
2022-12-19 15:09:36 +0700

    x

--- public/js/xuly8.js
+++ public/js/xuly8.js
@@ -77,41 +77,46 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-            </div>
-        );
-    }
-}
-
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Board />,
     document.getElementById('root')
 );
 
 function calculateWinner(squares) {
     const lines = [
-        [0, 1, 2, 3],
-        [4, 5, 6, 7],
-        [8, 9, 10, 11],
-        [12, 13, 14, 15],
-        [0, 4, 8, 12],
-        [1, 5, 9, 13],
-        [2, 6, 10, 14],
-        [3, 7, 11, 15],
-        [3, 6, 9, 12],
-        [0, 5, 10, 15],
+        [0, 1, 2],
+        [1, 2, 3],
+        [4, 5, 6],
+        [5, 6, 7],
+        [8, 9, 10],
+        [9, 10, 11],
+        [12, 13, 14],
+        [13, 14, 15],
+
+        [0, 4, 8],
+        [4, 8, 12],
+        [1, 5, 9],
+        [5, 9, 13],
+        [2, 6, 10],
+        [6, 10, 14],
+        [3, 7, 11],
+        [7, 11, 15],
+
+        [0, 5, 10],
+        [5, 10, 15],
+        [3, 6, 9],
+        [6, 9, 12],
+
+        [2, 5, 8],
+        [7, 10, 13],
+        [1, 6, 11],
+        [4, 9, 14]
     ];
     for (let i = 0; i < lines.length; i++) {
-        const [a, b, c, d] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
     }
